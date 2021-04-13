aec336b8010f93a6f578eb0effe0a6dcf338e2f8
Luke Bennett <hello@lukebennett.com.au>
2021-04-13 10:02:20 +1000

    Update page/layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import { SEO } from '../components/seo';
 
 function IndexPage(): React.ReactElement {
   return (
-    <Layout>
-      <div className="w-full px-4 py-12 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
-        <SEO title="Home" />
-        <h1>Hello World</h1>
-      </div>
-    </Layout>
+    <>
+      <SEO title="Home" />
+      <Layout>
+        <div className="w-full px-4 py-12 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
+          <h1>Hello World</h1>
+        </div>
+      </Layout>
+    </>
   );
 }
 
